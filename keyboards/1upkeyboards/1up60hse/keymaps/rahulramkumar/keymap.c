991a7333d15a9a9df860bd2b5b5e58baa2e4c4bc
Rahul Ramkumar <rahramku@twitch.tv>
2020-06-21 22:31:54 -0600

    updating

--- keyboards/1upkeyboards/1up60hse/keymaps/rahulramkumar/keymap.c
+++ keyboards/1upkeyboards/1up60hse/keymaps/rahulramkumar/keymap.c
@@ -16,28 +16,60 @@
  */
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    NINJA
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record){
+    switch(keycode){
+        case NINJA:
+            if(record->event.pressed){
+                SEND_STRING("Ninja literally made gaming mainstream. Before him people looked down on gaming like it was only for nerds or whatever. He made it okay for everyone to play video games without beings called a virgin or nerd.");
+            } else {
+
+            }
+            break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_60_ansi(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
     KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
+    MO(1), KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
   [1] = LAYOUT_60_ansi(
     KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
     KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_HOME, KC_PGUP, KC_TRNS,
-    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_PGDN, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(2), KC_TRNS, KC_TRNS
+    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TILD, KC_TRNS, KC_INS, KC_HOME, KC_PGUP, KC_TRNS,
+    KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_END, KC_PGDN, KC_TRNS,
+    KC_TRNS, MO(2), KC_TRNS, KC_TRNS, KC_TRNS, MO(2), KC_TRNS, KC_TRNS
   ),
 
   [2] = LAYOUT_60_ansi(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
+    KC_TRNS, NINJA, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
     BL_TOGG, BL_INC, BL_DEC, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
+};
+
+enum {
+    TD_TAPTAP = 0
+};
+
+void taptap (qk_tap_dance_state_t *state, void *user_data){
+    if (state->count >=5) {
+        SEND_STRING ("This is a test");
+        reset_tap_dance(state);
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_TAPTAP] = ACTION_TAP_DANCE_FN (taptap)
 };
